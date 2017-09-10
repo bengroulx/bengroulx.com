531d6dfa10863f84a1194c6b9aeb81cc60c38b81
Ben Groulx <ben@bengroulx.com>
2017-09-10 14:52:24 -0700

    SW -v6

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,15 +1,17 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v5';
+const PRECACHE = 'precache-v6';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
     '../../index.html',
-    '../../', // Alias for index.html
+    '../../', // alias
     '../css/main.css',
     '../fonts/PitchWeb-Regular.woff',
+    'https://bengroulx.com',
+    'https://bengroulx.com/index.html' // alias
 ];
 
 // The install handler takes care of precaching the resources we always need.
